2acb0a677c94c9d17cc8347d018e68ecb4d471b0
Chance <phs811@naver.com>
2022-01-20 17:52:22 +0900

    WritePage build

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:fastcampus_diary/data/utils.dart';
+import 'package:fastcampus_diary/write.dart';
 import 'package:flutter/material.dart';
 
 import 'data/todo.dart';
@@ -35,9 +37,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // 여러개를 담는 List를 통해 idx(할일)이 1이상일 때
   // 무엇을 담냐면 todo.dart의 여러 정보들을 담아줌
-  List<Todo> todos = [];
-
-
+  List<Todo> todos = [
+    Todo(
+      title : "패스트캠퍼스 강의듣기",
+      memo : "앱개발 입문 강의 듣기",
+      color : Colors.redAccent.value,
+      done : 0,
+      category: "공부",
+      date : 20210709
+    ),
+    Todo(
+        title : "패스트캠퍼스 강의듣기2",
+        memo : "앱개발 입문 강의 듣기",
+        color : Colors.blue.value,
+        done : 1,
+        category: "공부",
+        date : 20210709
+    ),
+  ];
 
   @override
   void initState(){
@@ -50,54 +67,120 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: PreferredSize(child : AppBar(),
         preferredSize: Size.fromHeight(0),
       ),
+      floatingActionButton: FloatingActionButton(
+        child : Icon(Icons.add , color : Colors.white,),
+        onPressed: () async {
+          // 화면 이동을 해야 합니다.
+          Todo todo = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (ctx) => TodoWritePage(todo : Todo(
+              title: "",
+              color : 0,
+              memo : "",
+              done: 0,
+              category: "",
+              date: Utils.getFormatTime(DateTime.now())
+            ))));
+
+          setState(() {
+            todos.add(todo);
+          });
+
+        },
+      ),
       body: ListView.builder(
-          itemBuilder: (ctx , idx){
-            if(idx == 0){
-              return Container(
-                child : const Text("오늘 하루" , style : TextStyle(fontSize: 20 , fontWeight: FontWeight.bold),),
-                margin : const EdgeInsets.symmetric(vertical: 12 , horizontal: 20),
-              );
-            }else if(idx == 1){
-              return Container(
+        itemBuilder: (ctx , idx){
+
+          if(idx == 0){
+            return Container(
+              child : const Text("오늘 하루" , style : TextStyle(fontSize: 20 , fontWeight: FontWeight.bold),),
+              margin : const EdgeInsets.symmetric(vertical: 12 , horizontal: 20),
+            );
+          }else if(idx == 1){
+
+            List<Todo> undone = todos.where((t){
+              return t.done == 0;
+            }).toList();
+
+            return Container(
                 child : Column(
-                  children: List.generate(todos.length, (_idx){
-                      // todos의 갯수 인덱스만큼 출력
-                      Todo t = todos[_idx];
+                    children: List.generate(undone.length, (_idx){
+                      Todo t = undone[_idx];
 
                       return Container(
-                        decoration: BoxDecoration(
-                          color : Color(t.color),
-                          borderRadius: BorderRadius.circular(16)
-                        ),
-                        padding : const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-                        margin : const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-                        child : Column(
-                          // 각 줄은 중앙 정렬로 기본 셋팅되어잇음
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          //Column의 경우 행이 들어가는데 행에 대한 얼라이먼트가 아닌 행의 열이 시작으로 되던지 가운데인지를
-                          // 크로스 얼라이언트를 통해 설정
+                          decoration: BoxDecoration(
+                              color : Color(t.color),
+                              borderRadius: BorderRadius.circular(16)
+                          ),
+                          padding : const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                          margin : const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                          child : Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
 
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(t.title , style: const TextStyle(fontSize: 18, color : Colors.white , fontWeight: FontWeight.bold),),
-                                Text(t.done == 0 ? "미완료" : "완료" , style: const TextStyle(color : Colors.white),)
-                              ],
-                            ),
-                            Container(height: 8), // 여백
-                            Text(t.memo , style: const TextStyle(color : Colors.white),)
-                          ],
-                        )
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(t.title , style: const TextStyle(fontSize: 18, color : Colors.white , fontWeight: FontWeight.bold),),
+                                  Text(t.done == 0 ? "미완료" : "완료" , style: const TextStyle(color : Colors.white),)
+                                ],
+                              ),
+                              Container(height: 8), // 여백
+                              Text(t.memo , style: const TextStyle(color : Colors.white),)
+                            ],
+                          )
+                      );
+                    }
+                    )
+                )
+            );
+          }
+          else if(idx == 2){
+            return Container(
+              child : const Text("완료된 하루" , style : TextStyle(fontSize: 20 , fontWeight: FontWeight.bold),),
+              margin : const EdgeInsets.symmetric(vertical: 12 , horizontal: 20),
+            );
+          }else if(idx == 3){
+
+            List<Todo> done = todos.where((t){
+              return t.done == 1;
+            }).toList();
+
+            return Container(
+                child : Column(
+                    children: List.generate(done.length, (_idx){
+                      Todo t = done[_idx];
+
+                      return Container(
+                          decoration: BoxDecoration(
+                              color : Color(t.color),
+                              borderRadius: BorderRadius.circular(16)
+                          ),
+                          padding : const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                          margin : const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                          child : Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(t.title , style: const TextStyle(fontSize: 18, color : Colors.white , fontWeight: FontWeight.bold),),
+                                  Text(t.done == 0 ? "미완료" : "완료" , style: const TextStyle(color : Colors.white),)
+                                ],
+                              ),
+                              Container(height: 8), // 여백
+                              Text(t.memo , style: const TextStyle(color : Colors.white),)
+                            ],
+                          )
                       );
                     }
                   )
                 )
-              );
-            }
+            );
+          }
 
-            return Container();
-          },
+          return Container();
+        },
         itemCount: 4,
       ),
       bottomNavigationBar: BottomNavigationBar(
---
+++ lib/write.dart
@@ -0,0 +1,162 @@
+//todolist를 작성하는 페이지
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'data/todo.dart';
+
+class TodoWritePage extends StatefulWidget{
+  // 글변경 카테고리 등등 때문에 stateful
+  final Todo todo;
+
+  const TodoWritePage({Key key , this.todo}) : super(key : key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TodoWritePageState();
+  }
+
+}
+
+class _TodoWritePageState extends State<TodoWritePage> {
+  // 실제 페이지 위젯을 만들어줄 클래스
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController memoController = TextEditingController();
+  //텍스트 필드를 감지하는 컨트롤러
+  int colorIndex = 0;
+  int ctIndex = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          // 위젯을 리스트로 가지고 있는 친구
+          TextButton(
+            child: const Text("저장" , style: TextStyle(color : Colors.white),),
+            onPressed: (){
+              // 페이지 저장 시 사용
+              // 텍스트필드 컨트롤러의 .text저장
+              widget.todo.title = nameController.text;
+              widget.todo.memo = memoController.text;
+
+              // 이 페이지가 꺼지면서 실행됫던 widget.todo를 넘김.
+              Navigator.of(context).pop(widget.todo);
+            },
+          )
+        ],
+      ),
+
+      body: // 휴대폰 환경이 다양하게 때문에 List가 스크롤이 되는 행을 제공하기 때문에 Column을 사용할 떄는 lsitviw를 쓰고 그 안에 Column을 쓰자
+      ListView.builder(
+        itemBuilder: (ctx , idx){
+          if(idx == 0){
+            return Container(
+              child : const Text("제목" , style: TextStyle(fontSize: 20, ),),
+              margin : const EdgeInsets.symmetric(vertical: 12 , horizontal: 16),
+
+            );
+
+            // 텍스트필드
+          }else if(idx == 1){
+            return Container(
+              child: TextField(
+                controller: nameController,
+              ),
+              margin : const EdgeInsets.symmetric(horizontal: 16),
+            );
+          }
+          // 색상
+          else if(idx == 2){
+            return InkWell(child : Container(
+              //InkWell은 해당 위젯을 선택가능하게 하기 위한 것 따라서 전체를 감싸줘야함 ()
+              margin : const EdgeInsets.symmetric(vertical: 12 , horizontal: 16),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children : [
+                    const Text("색상" , style: TextStyle(fontSize: 20),),
+                    Container(
+                      width: 15,
+                      height: 15,
+                      color: Color(widget.todo.color),
+                      // 색상 접근 시 그냥 위에 클래스에서 선언한 todo.color로 접근하면 안된다.
+                      // 왜냐면 상위 클래스에 접근하기 위해선 widget을 앞에 꼭 붙여줘야 하기 때문
+
+                    )
+                  ]
+              ),
+            ),
+              onTap: (){
+                List<Color> colors = [
+                  Color(0xff80d3f4),
+                  Color(0xffd180f4),
+                  Color(0xff803175),
+                  Color(0xffb4414f),
+                  Color(0xfff4e580),
+                  Color(0xff91f480),
+                  Color(0xffd0e3ea),
+                ];
+
+                widget.todo.color = colors[colorIndex].value;
+                colorIndex++;
+                //7이 넘어가지 않게 하기 위해
+                setState((){
+                  colorIndex = colorIndex % colors.length;
+                });
+              },
+            );
+
+          }
+          else if(idx == 3){
+            return InkWell(child : Container(
+              margin : const EdgeInsets.symmetric(vertical: 12 , horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("카테고리", style: TextStyle(fontSize: 20)),
+                  Text(widget.todo.category)
+                ],
+              ),
+            ),
+              onTap: (){
+                List<String> category = ["공부" , "운동" , "게임"];
+
+                widget.todo.category = category[ctIndex];
+                ctIndex++;
+                setState(() {
+                  ctIndex = ctIndex % category.length;
+                });
+              },
+              
+            );
+          }
+          else if(idx == 4){
+            return Container(
+              margin : const EdgeInsets.symmetric(vertical: 12 , horizontal: 16),
+              child: const Text("메모", style: TextStyle(fontSize: 20)),
+            );
+          }
+          else if(idx == 5){
+            return Container(
+              margin : const EdgeInsets.symmetric(vertical: 1 , horizontal: 16),
+              child: TextField(
+                controller: memoController,
+                // 여러 줄 입력
+                maxLines: 10,
+                minLines: 10,
+                decoration: const InputDecoration(
+                    border : OutlineInputBorder(borderSide: BorderSide(color : Colors.black))
+                ),
+              ),
+            );
+          }
+          // 아이템 6개 이외의 값이 오면 그냥컨테이너 리턴
+          return Container();
+        },
+        itemCount: 6,
+      ) ,
+    );
+  }
+
+}
